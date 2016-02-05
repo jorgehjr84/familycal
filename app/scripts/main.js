021974d3e98d6574274dda22754fb87a9c79a51c
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2016-02-05 09:43:30 -0500

    Wired up the beginnings of ngRoute

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,18 @@
-var familyCal = angular.module('familyCal',['ngMaterial']);
+'use strict';
+var familyCal = angular.module('familyCal',['ngMaterial','ngRoute']);
 
 
-// familyCal.controller('MainController', MainController){
-//
-// }
+familyCal.config(function($routeProvider){
+$routeProvider
+  .when('/',
+      {
+        controller: 'MainController',
+        templateUrl: 'views/main_page.html'
+      })
+     .when('/sign_up',
+      {
+            controller: 'MainController',
+          templateUrl: 'views/sign_up.html'
+      })
+      .otherwise({ redirectTo: '/'});
+});
