39fc46c45625e9be473b79d3d9814d328e8db5c1
Jorge Hernandez <jorge_l_hernandez@yahoo.com>
2016-02-12 00:00:22 -0500

    Wohoo. Pushing data to Firebase now
    Finished with the submit controller, now we can push a new user to Firebase. Need to work on requirements for the form data. Also created the add User function.

--- app/scripts/submitcontroller.js
+++ app/scripts/submitcontroller.js
@@ -2,9 +2,26 @@
   angular.module('familyCal')
   .controller('SubmitController', submitCtrl);
 
-  function submitCtrl ($firebase, FIREBASE_URL){
+  function submitCtrl ($firebase, FIREBASE_URL, $firebaseArray){
 
-    this.user = {};
-      var ref = new Firebase('https://familycal.firebaseIO.com/' + '/users');
 
-}
+  var self = this;
+  var ref = new Firebase('https://familycal.firebaseIO.com/' + '/users');
+
+    self.users = $firebaseArray(ref);
+
+        self.addUser = function(){
+
+                self.users.$add({
+                  firstName: self.newUserFirstName,
+                  lastName:  self.newUserLastName,
+                  email:     self.newUserEmail,
+                  password:  self.newUserPassword
+                });
+
+               self.newUserFirstName = '';
+               self.newUserLastName = '';
+               self.newUserEmail = '';
+               self.newUserPassword = '';
+          }
+  };
